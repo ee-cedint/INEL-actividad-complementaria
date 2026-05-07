1eb87413d140921b5712e5d458f703f3d15f10e6
Igor Gómez <igor.gomez@upm.es>
2026-05-07 14:11:03 +0200

    Nueva versión de las actividades de la sesion 2
    En la actividad 1 se usa el MOSFET con la señal digital D12
    En la actividad 2 se usa el BJT con la señal PWM de D9
    Se añade parpadeo rápido del led (D13) para comprobar que se está
    ejecutando el sketch.
    Se deja de leer y mostrar la última señal analógica (A3) pues no se
    estaba usando

--- arduino/sesion2/INEL26_S2_A1_A2/INEL26_S2_A1_A2.ino
+++ arduino/sesion2/INEL26_S2_A1_A2/INEL26_S2_A1_A2.ino
@@ -3,19 +3,17 @@
 // Compatible con Arduino Uno R3 y Uno R4 Minima 
 // cambiando BOARD_IS_R4=true/false
 //
-// ACTIVIDAD 1 (BJT):
-//   D13 -> Rin -> base
-//   A0 = Vb
-//   A1 = Ve
-//   A2 = Vc
-//   A3 = Vcc (opcional)
-//
-// ACTIVIDAD 2 (MOSFET PWM):
-//   D9 -> gate
+// ACTIVIDAD 1 (MOSFET):
+//   D12 -> Rin -> gate
 //   A0 = Vg
 //   A1 = Vd
-//   A2 = Vled o Vr (opcional)
-//   A3 = Vcc (opcional)
+//   A2 = Vcc   
+//
+// ACTIVIDAD 2 (BJT PWM):
+//   D9 -> base
+//   A0 = Vb
+//   A1 = Vc
+//   A2 = Vled o Vr
 //
 // COMANDOS SERIE:
 //   h        ayuda
@@ -34,7 +32,7 @@
 //   t       en modo MOSFET: siguiente PWM
 //
 // Salida periódica:
-//   A0<TAB>A1<TAB>A2<TAB>A3
+//   A0<TAB>A1<TAB>A2
 // ============================================================
 
 
@@ -61,27 +59,33 @@ static const float VREF_VOLTS = 5.0f;
 // Resolución ADC según placa
 static const uint8_t ADC_BITS = BOARD_IS_R4 ? 12 : 10;
 
+// Tiempos On y Off de parpadeo del Led en milisegundos
+static const uint32_t LED_MILIS_ON = 100;
+static const uint32_t LED_MILIS_OFF = 200;
+static const uint32_t LED_SAMPLES_ON = (LED_MILIS_ON * SAMPLE_RATE_HZ) / 1000;
+static const uint32_t LED_SAMPLES_OFF = (LED_MILIS_OFF * SAMPLE_RATE_HZ) / 1000;
+
 // ------------------------------------------------
 
 
 // Pines analógicos
-static const uint8_t CH_PINS[4] = {A0, A1, A2, A3};
+static const uint8_t CH_PINS[3] = {A0, A1, A2};
 
 // Pines de control
-static const uint8_t BJT_PIN = 13;      // Actividad 1
-static const uint8_t MOSFET_PIN = 9;    // Actividad 2 (PWM)
+static const uint8_t MOSFET_PIN = 12;    // Actividad 1
+static const uint8_t BJT_PIN = 9;        // Actividad 2 (PWM)
 
 // Parámetros derivados
 static const uint16_t ADC_MAX = (1u << ADC_BITS) - 1;
 
 // Estados
 enum Mode {
-  MODE_BJT = 1,
-  MODE_MOSFET = 2
+  MODE_MOSFET = 1,
+  MODE_BJT = 2
 };
 
 static Mode currentMode = MODE_MOSFET;
-static bool bjtState = false;
+static bool mosfetState = false;
 static uint8_t pwmPercent = 0;
 
 
@@ -97,12 +101,12 @@ static inline uint8_t pwmPercentToValue(uint8_t percent) {
 }
 
 void applyOutputs() {
-  if (currentMode == MODE_BJT) {
-    digitalWrite(BJT_PIN, bjtState ? HIGH : LOW);
-    analogWrite(MOSFET_PIN, 0);
+  if (currentMode == MODE_MOSFET) {
+    digitalWrite(MOSFET_PIN, mosfetState ? HIGH : LOW);
+    analogWrite(BJT_PIN, 0);
   } else {
-    digitalWrite(BJT_PIN, LOW);
-    analogWrite(MOSFET_PIN, pwmPercentToValue(pwmPercent));
+    digitalWrite(MOSFET_PIN, LOW);
+    analogWrite(BJT_PIN, pwmPercentToValue(pwmPercent));
   }
 }
 
@@ -110,12 +114,12 @@ void printStatus() {
   Serial.println();
   Serial.println(F("----- ESTADO -----"));
   Serial.print(F("Modo: "));
-  if (currentMode == MODE_BJT) {
-    Serial.println(F("Actividad 1 - BJT"));
-    Serial.print(F("D13 = "));
-    Serial.println(bjtState ? F("HIGH") : F("LOW"));
+  if (currentMode == MODE_MOSFET) {
+    Serial.println(F("Actividad 1 - MOSFET"));
+    Serial.print(F("D12 = "));
+    Serial.println(mosfetState ? F("HIGH") : F("LOW"));
   } else {
-    Serial.println(F("Actividad 2 - MOSFET PWM"));
+    Serial.println(F("Actividad 2 - BJT PWM"));
     Serial.print(F("PWM D9 = "));
     Serial.print(pwmPercent);
     Serial.println(F("%"));
@@ -131,6 +135,7 @@ void printStatus() {
   Serial.println(VREF_VOLTS, 3);
   Serial.println(F("------------------"));
   Serial.println();
+
 }
 
 void printHelp() {
@@ -138,15 +143,15 @@ void printHelp() {
   Serial.println(F("=== COMANDOS ==="));
   Serial.println(F("h        -> ayuda"));
   Serial.println(F("s        -> estado actual"));
-  Serial.println(F("m1       -> modo actividad 1 (BJT, D13)"));
-  Serial.println(F("m2       -> modo actividad 2 (MOSFET PWM, D9)"));
+  Serial.println(F("m1       -> modo actividad 1 (MOSFET, D12)"));
+  Serial.println(F("m2       -> modo actividad 2 (BJT PWM, D9)"));
   Serial.println();
-  Serial.println(F("Modo BJT:"));
-  Serial.println(F("  0      -> D13 LOW"));
-  Serial.println(F("  1      -> D13 HIGH"));
-  Serial.println(F("  t      -> toggle D13"));
+  Serial.println(F("Modo MOSFET:"));
+  Serial.println(F("  0      -> D12 LOW"));
+  Serial.println(F("  1      -> D12 HIGH"));
+  Serial.println(F("  t      -> toggle D12"));
   Serial.println();
-  Serial.println(F("Modo MOSFET PWM:"));
+  Serial.println(F("Modo BJT PWM:"));
   Serial.println(F("  a     -> PWM 0%"));
   Serial.println(F("  b     -> PWM 25%"));
   Serial.println(F("  c     -> PWM 50%"));
@@ -154,7 +159,7 @@ void printHelp() {
   Serial.println(F("  e     -> PWM 100%"));
   Serial.println(F("  t     -> siguiente PWM: 0,25,50,75,100,0..."));
   Serial.println();
-  Serial.println(F("Salida: A0<TAB>A1<TAB>A2<TAB>A3"));
+  Serial.println(F("Salida: A0<TAB>A1<TAB>A2"));
   Serial.println();
 }
 
@@ -173,44 +178,44 @@ void handleCommand(String cmd) {
   }
 
   if (cmd == "m1") {
-    currentMode = MODE_BJT;
+    currentMode = MODE_MOSFET;
     applyOutputs();
-    Serial.println(F("Modo cambiado a Actividad 1 (BJT)"));
+    Serial.println(F("Modo cambiado a Actividad 1 (MOSFET)"));
     return;
   }
 
   if (cmd == "m2") {
-    currentMode = MODE_MOSFET;
+    currentMode = MODE_BJT;
     applyOutputs();
-    Serial.println(F("Modo cambiado a Actividad 2 (MOSFET PWM)"));
+    Serial.println(F("Modo cambiado a Actividad 2 (BJT PWM)"));
     return;
   }
 
-  if (currentMode == MODE_BJT) {
+  if (currentMode == MODE_MOSFET) {
     if (cmd == "0") {
-      bjtState = false;
+      mosfetState = false;
       applyOutputs();
-      Serial.println(F("D13 = LOW"));
+      Serial.println(F("D12 = LOW"));
       return;
     }
 
     if (cmd == "1") {
-      bjtState = true;
+      mosfetState = true;
       applyOutputs();
-      Serial.println(F("D13 = HIGH"));
+      Serial.println(F("D12 = HIGH"));
       return;
     }
 
     if (cmd == "t" || cmd == "T") {
-      bjtState = !bjtState;
+      mosfetState = !mosfetState;
       applyOutputs();
-      Serial.print(F("D13 = "));
-      Serial.println(bjtState ? F("HIGH") : F("LOW"));
+      Serial.print(F("D12 = "));
+      Serial.println(mosfetState ? F("HIGH") : F("LOW"));
       return;
     }
   }
 
-  if (currentMode == MODE_MOSFET) {
+  if (currentMode == MODE_BJT) {
     if (cmd == "a") {
       pwmPercent = 0;
       applyOutputs();
@@ -279,9 +284,11 @@ void setup() {
 
   pinMode(BJT_PIN, OUTPUT);
   pinMode(MOSFET_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 
-  digitalWrite(BJT_PIN, LOW);
-  analogWrite(MOSFET_PIN, 0);
+  digitalWrite(MOSFET_PIN, LOW);
+  analogWrite(BJT_PIN, 0);
 
 #if defined(ARDUINO_UNOR4_MINIMA) || defined(ARDUINO_UNOR4_WIFI)
   if (BOARD_IS_R4) {
@@ -290,7 +297,7 @@ void setup() {
 #endif
 
   // Warm-up ADC
-  for (uint8_t i = 0; i < 4; i++) {
+  for (uint8_t i = 0; i < 3; i++) {
     (void)analogRead(CH_PINS[i]);
   }
 
@@ -311,11 +318,13 @@ void loop() {
   const uint32_t outputIntervalMs = 1000UL / OUTPUT_RATE_HZ;
   static uint32_t lastOutputMs = millis();
 
+  static bool ledStateOn = false;
+
   static uint32_t sum0 = 0;
   static uint32_t sum1 = 0;
   static uint32_t sum2 = 0;
-  static uint32_t sum3 = 0;
   static uint32_t n = 0;
+  static uint32_t countSamples = 0; // Para el parpadeo rápido del led
 
   uint32_t nowUs = micros();
   if ((int32_t)(nowUs - nextSampleUs) >= 0) {
@@ -324,8 +333,24 @@ void loop() {
     sum0 += (uint32_t)analogRead(CH_PINS[0]);
     sum1 += (uint32_t)analogRead(CH_PINS[1]);
     sum2 += (uint32_t)analogRead(CH_PINS[2]);
-    sum3 += (uint32_t)analogRead(CH_PINS[3]);
     n++;
+
+    // Parpadeo Led
+    countSamples++;
+    if (ledStateOn) {
+      if (countSamples > LED_SAMPLES_ON) {
+        digitalWrite(LED_BUILTIN, LOW);
+        ledStateOn = false;
+        countSamples = 0;
+      }
+    }
+    else {
+      if (countSamples > LED_SAMPLES_OFF) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        ledStateOn = true;
+        countSamples = 0;
+      }      
+    }
   }
 
   uint32_t nowMs = millis();
@@ -337,29 +362,24 @@ void loop() {
     float avg0 = (float)sum0 / (float)n;
     float avg1 = (float)sum1 / (float)n;
     float avg2 = (float)sum2 / (float)n;
-    float avg3 = (float)sum3 / (float)n;
 
     sum0 = 0;
     sum1 = 0;
     sum2 = 0;
-    sum3 = 0;
     n = 0;
 
     if (!OUTPUT_VOLTS) {
       Serial.print((int)(avg0 + 0.5f)); Serial.print('\t');
       Serial.print((int)(avg1 + 0.5f)); Serial.print('\t');
-      Serial.print((int)(avg2 + 0.5f)); Serial.print('\t');
-      Serial.println((int)(avg3 + 0.5f));
+      Serial.println((int)(avg2 + 0.5f));
     } else {
       float v0 = countsToVolts(avg0);
       float v1 = countsToVolts(avg1);
       float v2 = countsToVolts(avg2);
-      float v3 = countsToVolts(avg3);
 
       Serial.print(v0, 3); Serial.print('\t');
       Serial.print(v1, 3); Serial.print('\t');
-      Serial.print(v2, 3); Serial.print('\t');
-      Serial.println(v3, 3);
+      Serial.println(v2, 3); 
     }
   }
 }
